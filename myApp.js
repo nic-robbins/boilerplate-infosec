232ccd40113490c22f115a3024d2673beb4c390d
Nic Robbins <nicmmrob@gmail.com>
2024-03-21 06:55:22 +0000

    hsts, no cache

--- myApp.js
+++ myApp.js
@@ -1,14 +1,16 @@
 let express = require("express");
 let app = express();
 let helmet = require("helmet");
+let ninetyDaysInSeconds = 90*24*60*60;
 
 app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-
-
+app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 
 
