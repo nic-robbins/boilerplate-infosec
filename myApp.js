21dd6b8acaffe035213ac35d6a3f0ff9a09601ea
Nic Robbins <nicmmrob@gmail.com>
2024-03-21 06:43:43 +0000

    no sniff

--- myApp.js
+++ myApp.js
@@ -4,6 +4,8 @@ let helmet = require("helmet");
 
 app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
 app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 
 
