51cf485664f55ffd17733bd0c89557bfb8ad0ca9
Nic Robbins <nicmmrob@gmail.com>
2024-03-21 06:33:30 +0000

    hide powered by

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const app = express();
+let express = require("express");
+let app = express();
+let helmet = require("helmet");
 
-
-
+app.use(helmet.hidePoweredBy({ setTo: "PHP 4.2.0" }));
 
 
 
